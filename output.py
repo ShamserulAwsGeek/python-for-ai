a2c792ce4bc3251feddd77fc665e0d00fbbdcb86
ShamserulAwsGeek <shamserul786@gmail.com>
2025-08-25 23:40:17 +0530

    file updated

--- output.py
+++ output.py
@@ -24,3 +24,11 @@ if 5 > 2:
 if 5 > 2:
  print("Five is greater than two!")
  print("Five is greater than two!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
