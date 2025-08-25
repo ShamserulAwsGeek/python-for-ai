5f8e67c01f622fcc103af3d14d1c05de38f7b84a
ShamserulAwsGeek <shamserul786@gmail.com>
2025-08-25 23:45:52 +0530

    file updated

--- variables.py
+++ variables.py
@@ -10,6 +10,23 @@
 # print(x)
 
 #Casting:
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+
+# x = "John"
+# # is the same as
+
+# x = 'John'
+
+
+#Variable names are case-sensitive:
+a = 4
+A = "Sally"
+#A will not overwrite a
