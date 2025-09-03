f1e7f86d0dc0b3aa13234c914fe18f838340f7db
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-03 23:29:48 +0530

    updated

---
+++ string.py
@@ -0,0 +1,34 @@
+# print("It's alright")
+# print("He is called 'Johnny'")
+# print('He is called "Johnny"')
+
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+
+# b = "Hello, World!"
+# print(b[2:6])
+
+# b = "Hello, World!"
+# print(b[:6])
+
+# b = "Hello, World!"
+# print(b[2:])
+
+
+# a = "Hello, World!"
+# print(a.upper())
+
+# b = "   Hello World!"
+# print(b.strip())
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
+
+
+a = "Hello, World!"
+print(a.split(","))
