a44d6bcfb5cc2913fa403e81afcc3367736463c8
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-04 22:24:31 +0530

    update

--- boolean.py
+++ boolean.py
@@ -1,13 +1,46 @@
-a = 200
-b = 33
+# a = 200
+# b = 33
 
-if b > a:
-  print("b is greater than a")
-else:
-  print("b is not greater than a")
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
 
 
 
-bool("abc")
-bool(123)
-bool(["apple", "cherry", "banana"])
+# bool("abc")
+# bool(123)
+# bool(["apple", "cherry", "banana"])
+
+
+
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+
+# print(bool("Hello"))
+# print(bool(15))
+
+
+# bool("abc")
+# bool(123)
+# bool(["apple", "cherry", "banana"])
+
+# print(bool("abc"))
+# print(bool(123))
+# print(bool(["apple", "cherry", "banana"]))
+
+# bool(False)
+# bool(None)
+# bool(0)
+# bool("")
+# bool(())
+# bool([])
+# bool({})
+
+# print(bool(False))
+# print(bool(0))
+# print(bool(""))
