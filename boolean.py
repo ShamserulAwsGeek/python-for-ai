7328b475bc725743848798a3e74b918c6615f566
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-04 22:33:47 +0530

    update

--- boolean.py
+++ boolean.py
@@ -44,3 +44,31 @@
 # print(bool(False))
 # print(bool(0))
 # print(bool(""))
+
+
+# class myclass():
+#   def __len__(self):
+#     return 0
+
+# myobj = myclass()
+# print(bool(myobj))
+
+# class awsclass():
+#   def time_duration(self):
+#     return 2
+  
+# awsobject = awsclass()
+# print(bool(awsobject))
+
+# def myFunction() :
+#     return False
+
+# print(myFunction())
+
+def myFunction() :
+    return False
+
+if myFunction():
+    print("YES!")
+else:
+    print("NO!")
