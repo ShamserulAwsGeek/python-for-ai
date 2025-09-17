d8930e28f94e48f491d0ea0091048c14572e2837
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-17 23:11:15 +0530

    updated

--- list.py
+++ list.py
@@ -108,6 +108,31 @@
 
 # print(newlist)
 
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort()
-print(thislist)
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# print(thislist)
+
+
+# def myfunc(n):
+#   return abs(n - 50)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key = myfunc)
+#print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
