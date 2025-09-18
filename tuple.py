759f1fcfd549a5f55bed1bd7bde5202728516053
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-18 23:16:53 +0530

    updated

--- tuple.py
+++ tuple.py
@@ -1,9 +1,53 @@
 # thistuple = ("apple", "banana", "cherry")
 # print(len(thistuple))
 
-thistuple = ("apple",)
-print(type(thistuple))
+# thistuple = ("apple",)
+# print(type(thistuple))
 
 #NOT a tuple
 # thistuple = ("apple")
 # print(type(thistuple))
+
+
+# mytuple = ("apple", "banana", "cherry")
+# print(type(mytuple))
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[-3])
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:5])
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[:4])
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:])
+
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[-4:-1])
+
+# thistuple = ("apple", "banana", "cherry")
+# if "apple" in thistuple:
+#   print("Yes, 'apple' is in the fruits tuple")
+
+
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
+# print(x)
+
+
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+z= list(thistuple)
+
+y.append("orange")
+z.append("mango")
+
+thistuple = tuple(y)
+thistuple = tuple(z)
+print(thistuple)
