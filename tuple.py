1a986494f2f28490a50b308a6c90fbfee8bde63c
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-18 23:34:56 +0530

    updated

--- tuple.py
+++ tuple.py
@@ -41,13 +41,71 @@
 
 
 
-thistuple = ("apple", "banana", "cherry")
-y = list(thistuple)
-z= list(thistuple)
+# thistuple = ("apple", "banana", "cherry")
+# y = list(thistuple)
+# z= list(thistuple)
 
-y.append("orange")
-z.append("mango")
+# y.append("orange")
+# z.append("mango")
 
-thistuple = tuple(y)
-thistuple = tuple(z)
-print(thistuple)
+# thistuple = tuple(y)
+# thistuple = tuple(z)
+# print(thistuple)
+
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+# (green, yellow, *red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+# fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+# (green, *tropic, red) = fruits
+
+# print(green)
+# print(tropic)
+# print(red)
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in thistuple:
+#   print(x)
+
+# thistuple = ("apple", "banana", "cherry")
+# for i in range(len(thistuple)):
+#   print(thistuple[i])
+
+# thistuple = ("amma", "abba", "bahan")
+# for i in range(len(thistuple)):
+#     print(thistuple[i])
+# print(i)
+
+
+# thistuple = ("apple", "banana", "cherry")
+# i = 0
+# while i < len(thistuple):
+#   print(thistuple[i])
+#   i = i + 1
+# print(i)
+
+# thistuple = ("Ashriti", "Maheen", "Farah")
+# i = 0
+# while i < len(thistuple):
+#     print(thistuple[i])
+#     i = i +1
+# print(i)    
+
+
+
+# fruits = ("apple", "banana", "cherry")
+# mytuple = fruits * 2
+
+# print(mytuple)
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
