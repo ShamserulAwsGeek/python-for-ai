d414ceb5bbde30646bb7180c34a1352ab34719b6
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-19 21:04:43 +0530

    updated

--- sets.py
+++ sets.py
@@ -20,3 +20,51 @@
 # print(set1)
 # print(set2)
 # print(set3)
+
+
+# thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# for x in thisset:
+#   print(x)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# print("apple" in thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.add("orange")
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# tropical = {"pineapple", "mango", "papaya"}
+
+# thisset.update(tropical)
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# mylist = ["kiwi", "orange"]
+
+# thisset.update(mylist)
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.remove("banana")
+
+# print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
