91cb4bb9c642cff3acdf8e8267e3de69adb4077e
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-21 13:14:29 +0530

    dict updated

--- dict.py
+++ dict.py
@@ -65,4 +65,80 @@
 
 # print(x) #after the change
 
+# x = car.values()
+# print(x) 
 
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.values()
+
+# print(x) #before the change
+
+# car["year"] = 2020
+
+# print(x) #after the change
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.values()
+# y = car.items()
+
+# print(x) #before the change
+
+# car["color"] = "red"
+
+# print(x) #after the change
+# print(y) 
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.items()
+
+# print(x) #before the change
+
+# car["year"] = 2020
+
+# print(x) #after the change
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.items()
+
+# print(x) #before the change
+
+# car["color"] = "red"
+
+# print(x) #after the change
+
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# if "model" in thisdict:
+#   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+# else:
+#     print("No, 'model' key dictionary mai nahi hai")
