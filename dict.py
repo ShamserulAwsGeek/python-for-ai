ae2a109a68b7c1479d40a8a3c87fc4fbcc845720
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-21 12:45:09 +0530

    dict updated

--- dict.py
+++ dict.py
@@ -22,3 +22,14 @@
 # print(thisdict)
 # print(len(thisdict))
 
+# thisdict = {
+#   "brand": "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }
+# print(thisdict)
+
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
