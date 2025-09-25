a6b68bb88c05b6c98b71aa2411ac39f84f97652f
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-25 12:38:27 +0530

    updated

--- function.py
+++ function.py
@@ -1,8 +1,8 @@
-# def my_function():
-#   print("Hello from a function")
+def my_function():
+  print("Hello from a function")
 
-# def my_function():
-#   print("Hello from a function")
+def my_function():
+  print("Hello from a function")
 
-# my_function()
+my_function()
 
