6234ca9ee31015d70b78b7189451222766003750
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-25 13:25:41 +0530

    updated

--- function.py
+++ function.py
@@ -1,8 +1,40 @@
-def my_function():
-  print("Hello from a function")
+# def my_function():
+#   print("Hello from a function")
 
-def my_function():
-  print("Hello from a function")
+# def my_function():
+#   print("Hello from a function")
 
-my_function()
+# my_function()
 
+# def my_function(fname):
+#   print(fname + " Shams")
+
+# my_function("Maheen")
+# my_function("Tobias")
+# my_function("Linus")
+
+
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Maheen", "Shams")
+
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Emil")
+
+# def my_function(*kids):
+#   print("The youngest child is " + kids[2])
+
+# my_function("Emil", "Tobias", "Linus")
+
+# def my_function(child3, child2, child1):
+#   print("The youngest child is " + child3)
+
+# my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+# def my_function(**kid):
+#   print("His last name is " + kid["lname"])
+
+# my_function(fname = "Tobias", lname = "Refsnes")
