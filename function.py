57a29405858cbbdebd920b7a227a5294750af213
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-26 21:10:32 +0530

    function updated

--- function.py
+++ function.py
@@ -39,10 +39,25 @@
 
 # my_function(fname = "Tobias", lname = "Refsnes")
 
-def my_function(country = "Norway"):
-  print("I am from " + country)
+# def my_function(country = "Norway"):
+#   print("I am from " + country)
 
-my_function("Sweden")
-my_function("India")
-my_function()
-my_function("Brazil")
+# my_function("Sweden")
+# my_function("India")
+# my_function()
+# my_function("Italy")
+
+# def my_function(food):
+#   for x in food:
+#     print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+
+# my_function(fruits)
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
