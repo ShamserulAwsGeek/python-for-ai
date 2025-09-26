8df7781e51656ba5ca55abd98cd536b1e9e32c6a
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-26 22:56:56 +0530

    decorator added

--- function.py
+++ function.py
@@ -94,13 +94,13 @@
 # my_function(5, 6, c = 7, d = 8)
 
 
-def tri_recursion(k):
-  if(k > 0):
-    result = k + tri_recursion(k - 1)
-    print(result)
-  else:
-    result = 0
-  return result
+# def tri_recursion(k):
+#   if(k > 0):
+#     result = k + tri_recursion(k - 1)
+#     print(result)
+#   else:
+#     result = 0
+#   return result
 
-print("Recursion Example Results:")
-tri_recursion(6)
+# print("Recursion Example Results:")
+# tri_recursion(6)
--- sets.py
+++ sets.py
@@ -104,8 +104,8 @@
 # myset = set1 | set2 | set3 |set4
 # print(myset)
 
-x = {"a", "b", "c"}
-y = (1, 2, 3)
+# x = {"a", "b", "c"}
+# y = (1, 2, 3)
 
-z = x.union(y)
-print(z)
+# z = x.union(y)
+# print(z)
