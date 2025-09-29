07f77961a61ee8b58b20cd81cac07b0a277932d3
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-29 13:34:05 +0530

    decorator updated

--- decorator.py
+++ decorator.py
@@ -43,18 +43,47 @@
 # print(myfunction())
 # print(otherfunction())
 
-def changecase(func):
-  def myfunc():
-    return func().lower()
-  return myfunc
+# def changecase(func):
+#   def myfunc():
+#     return func().lower()
+#   return myfunc
 
-@changecase
-def lowerfunction():
-  return "I AM BENGALI "
+# @changecase
+# def lowerfunction():
+#   return "I AM BENGALI "
 
-@changecase
-def otherlowerfunction():
-  return "I AM FROM MALDA"
+# @changecase
+# def otherlowerfunction():
+#   return "I AM FROM MALDA"
 
-print(lowerfunction())
-print(otherlowerfunction())
+# print(lowerfunction())
+# print(otherlowerfunction())
+
+
+# def changecase(func):
+#   def myinner(x):
+#     return func(x).upper()
+#   return myinner
+
+# @changecase
+# def myfunction(nam):
+#   return "Hello " + nam
+
+# print(myfunction("John"))
+
+
+# def changecase(func):
+#   def myinner(*args, **kwargs):
+#     return func(*args, **kwargs).upper()
+#   return myinner
+
+# @changecase
+# def myfunction(fnam):
+#   return "first name " + fnam
+
+# @changecase
+# def otherfunction(lnam):
+#   return "last name "  +  lnam
+
+# print(myfunction("Sham"))
+# print(otherfunction("Maheen"))
