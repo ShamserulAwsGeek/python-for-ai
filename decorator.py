760bd80be54abb395c9faf69dd4f78926f71da15
ShamserulAwsGeek <shamserul786@gmail.com>
2025-09-29 13:39:26 +0530

    decorator updated

--- decorator.py
+++ decorator.py
@@ -87,3 +87,39 @@
 
 # print(myfunction("Sham"))
 # print(otherfunction("Maheen"))
+
+
+# def changecase(n):
+#   def changecase(func):
+#     def myinner():
+#       if n == 1:
+#         a = func().lower()
+#       else:
+#         a = func().upper()
+#       return a
+#     return myinner
+#   return changecase
+
+# @changecase(1)
+# def myfunction():
+#   return "Hello Linus"
+
+# print(myfunction())
+
+
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+def addgreeting(func):
+  def myinner():
+    return "Hello " + func() + " Have a good day!"
+  return myinner
+
+@changecase
+@addgreeting
+def myfunction():
+  return "Tobias"
+
+print(myfunction())
