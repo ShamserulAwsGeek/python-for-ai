f3bfbc32eca7d734493f0493b56fcf3d16d8bce9
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-03 22:38:25 +0530

    added range

--- lambda.py
+++ lambda.py
@@ -26,11 +26,11 @@
 
 # print(mytripler(11))
 
-def myfunc(n):
-  return lambda a : a * n
+# def myfunc(n):
+#   return lambda a : a * n
 
-mydoubler = myfunc(2)
-mytripler = myfunc(3)
+# mydoubler = myfunc(2)
+# mytripler = myfunc(3)
 
-print(mydoubler(11))
-print(mytripler(11))
+# print(mydoubler(11))
+# print(mytripler(11))
