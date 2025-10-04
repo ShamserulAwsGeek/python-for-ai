ce7b51f5e95e33656bbf8c3672efa4aa511fa85b
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-04 11:55:21 +0530

    class created

--- class.py
+++ class.py
@@ -1,6 +1,18 @@
-#Create a class with name myclass and a variable x with value 4:
-class myclass():
-    x =4
-#Create an object p1 of myclass and print the value of x:
-p1 = myclass()
-print(p1.x)
+# #Create a class with name myclass and a variable x with value 4:
+# class myclass():
+#     x =4
+# #Create an object p1 of myclass and print the value of x:
+# p1 = myclass()
+# print(p1.x)
+
+
+#Create a class named Person, use the __init__() method to assign values for name and age:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
