82f05d1cede2ea18941bb03c41afc958397b4a0f
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-04 12:29:25 +0530

    class created

--- class.py
+++ class.py
@@ -37,15 +37,46 @@
 
 
 #The string representation of an object WITH the __str__() method:
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
     
-    def __str__(self):
-        return f"{self.name}({self.age})"
+#     def __str__(self):
+#         return f"{self.name}({self.age})"
 
-p1 = Person("Sham", 29)
-print(p1)
+# p1 = Person("Sham", 29)
+# print(p1)
+
+#Insert a function that prints a greeting, and execute it on the p1 object:
+# class Person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#     def myfunc(self):
+#         print("Hello my name is " + self.name)
+#         print("My age is " + str(self.age))
+    
+# p1 = Person("Shams", 29)
+# p1.myfunc()
+
+
+#Use the words mysillyobject and abc instead of self:
+# class Person:
+#   def __init__(mysillyobject, name, age):
+#     mysillyobject.name = name
+#     mysillyobject.age = age
+
+#   def myfunc(abc):
+#     print("Hello my name is " + abc.name)
+#     print("My age is " + str(abc.age))
+
+# p1 = Person("Shams", 29)
+# p1.age = 30
+# p1.myfunc()
+
+
+
+
 
 
