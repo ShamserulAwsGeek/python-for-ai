ffb8cae5c42b08f959bf650b4f900564e3bc6dfd
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-04 12:07:00 +0530

    class created

--- class.py
+++ class.py
@@ -7,12 +7,45 @@
 
 
 #Create a class named Person, use the __init__() method to assign values for name and age:
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("John", 36)
+
+# print(p1.name)
+# print(p1.age)
+
+# class shams:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# l1 = shams("Maheen", 25)
+# print(l1.name)
+# print(l1.age)
+
+#The string representation of an object WITHOUT the __str__() method:
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("John", 36)
+
+# print(p1)
+
+
+#The string representation of an object WITH the __str__() method:
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f"{self.name}({self.age})"
 
-p1 = Person("John", 36)
+p1 = Person("Sham", 29)
+print(p1)
 
-print(p1.name)
-print(p1.age)
+
