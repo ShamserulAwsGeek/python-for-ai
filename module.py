f8cc73942d10071d7ae98504b1b487e04e37b5ef
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-05 22:03:38 +0530

    module added

--- module.py
+++ module.py
@@ -1,7 +1,45 @@
+# def greeting(name):
+#   print("Hello, " + name)
+
+# import module
+
+# module.greeting("ShamsMaheen")
+
+
+# person1 = {
+#   "name": "John",
+#   "age": 36,
+#   "country": "Norway"
+# }
+
+# import module
+# a = module.person1["age"]
+# print(a)
+
+# import module as mx
+# a = mx.person1["age"]
+# print(a)
+
+
+# import platform
+# x = platform.system()
+# print(x)
+
+#List all the defined names belonging to the platform module:
+# import platform
+
+# x = dir(platform)
+# print(x)
+
+
 def greeting(name):
   print("Hello, " + name)
 
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
 
-import mymodule
-
-mymodule.greeting("Shamserul")
+from module import person1
+print(person1["country"])
