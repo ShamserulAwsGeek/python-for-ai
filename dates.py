50015bbe31a73a7ff3a3029e991ffadb317d6874
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-06 11:50:15 +0530

    datetime module added

--- dates.py
+++ dates.py
@@ -1,5 +1,29 @@
 #Import the datetime module and display the current date:
+# import datetime
+
+# x = datetime.datetime.now()
+# print(x)
+
+#Return the year and name of weekday:
+# import datetime
+
+# x = datetime.datetime.now()
+
+# print(x.year)
+# print(x.strftime("%A"))
+# print(x.day)
+# print(x.month)
+
+#Create a date object:
+# import datetime
+
+# x = datetime.datetime(2020, 5, 17)
+
+# print(x)
+
+#Display the name of the month:
 import datetime
 
-x = datetime.datetime.now()
-print(x)
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))
