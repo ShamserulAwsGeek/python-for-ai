e02de5ea51a53627503b22eb255f3319c1a78fb6
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-12 12:46:17 +0530

    machine learning added

--- machine-learning.py
+++ machine-learning.py
@@ -5,3 +5,11 @@ speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 x = numpy.mean(speed)
 
 print(x)
+
+import numpy
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = numpy.median(speed)
+
+print(x)
