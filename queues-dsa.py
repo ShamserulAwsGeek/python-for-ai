525c5e2cedc52456f5812e9229a8cf346a44d49e
ShamserulAwsGeek <shamserul786@gmail.com>
2025-10-14 23:14:53 +0530

    queue added

--- queues-dsa.py
+++ queues-dsa.py
@@ -1,25 +1,63 @@
 #Using a Python list as a queue:
-queue = []
+# queue = []
 
-# Enqueue
-queue.append('A')
-queue.append('B')
-queue.append('C')
-print("Queue: ", queue)
+# # Enqueue
+# queue.append('A')
+# queue.append('B')
+# queue.append('C')
+# print("Queue: ", queue)
 
-# Peek
-frontElement = queue[0]
-print("Peek: ", frontElement)
+# # Peek
+# frontElement = queue[0]
+# print("Peek: ", frontElement)
 
-# Dequeue
-poppedElement = queue.pop(0)
-print("Dequeue: ", poppedElement)
+# # Dequeue
+# poppedElement = queue.pop(0)
+# print("Dequeue: ", poppedElement)
 
-print("Queue after Dequeue: ", queue)
+# print("Queue after Dequeue: ", queue)
 
-# isEmpty
-isEmpty = not bool(queue)
-print("isEmpty: ", isEmpty)
+# # isEmpty
+# isEmpty = not bool(queue)
+# print("isEmpty: ", isEmpty)
 
-# Size
-print("Size: ", len(queue))
+# # Size
+# print("Size: ", len(queue))
+
+#Using a Python class as a queue:
+class Queue:
+  def __init__(self):
+    self.queue = []
+    
+  def enqueue(self, element):
+    self.queue.append(element)
+
+  def dequeue(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    return self.queue.pop(0)
+
+  def peek(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    return self.queue[0]
+
+  def isEmpty(self):
+    return len(self.queue) == 0
+
+  def size(self):
+    return len(self.queue)
+
+# Create a queue
+myQueue = Queue()
+
+myQueue.enqueue('A')
+myQueue.enqueue('B')
+myQueue.enqueue('C')
+
+print("Queue: ", myQueue.queue)
+print("Peek: ", myQueue.peek())
+print("Dequeue: ", myQueue.dequeue())
+print("Queue after Dequeue: ", myQueue.queue)
+print("isEmpty: ", myQueue.isEmpty())
+print("Size: ", myQueue.size())
