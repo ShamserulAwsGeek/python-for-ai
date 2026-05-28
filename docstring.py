19bb836e8a7e513d24bf18d74ca4772513b94517
Md Shamserul Haque <shamserul786@gmail.com>
2026-05-28 23:23:03 +0530

    Update docstring.py

--- docstring.py
+++ docstring.py
@@ -1,5 +1,5 @@
 
-
+#docstring should be just below function or class:
 def square(n):
   ''' Takes a number in n and returs square of n '''
   print(n**2)
@@ -7,3 +7,28 @@ def square(n):
 square(5)
 
 print(square.__doc__)
+
+#PEP 8:
+import this
+import this
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
