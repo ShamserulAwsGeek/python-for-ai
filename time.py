2a6175b04bb7462fe0fbe035f9d413ae248a061d
Md Shamserul Haque <shamserul786@gmail.com>
2026-05-28 22:16:42 +0530

    time update

--- time.py
+++ time.py
@@ -2,3 +2,19 @@
 import time
 timestamp = time.strftime('%H:%M:%S')
 print(timestamp)
+
+#Decide part of the day:
+import time
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+hour = int(input("enter the hour:"))
+print(hour)
+
+if(hour>=0 and hour<12):
+  print("Good Morning!")
+
+elif(hour>=12 and hour< 17):
+  print("Good Evening!")
+
+else:
+  print("Good night!")
