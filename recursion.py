377b11a9265d40f5743100a16757f1449cfbbddd
Md Shamserul Haque <shamserul786@gmail.com>
2026-05-29 20:57:41 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,27 +1,10 @@
-#PEP8 : python enhancement proposals.
->>> import this
-The Zen of Python, by Tim Peters
+def factorial(n):
+  if(n==0 or n==1):
+    return 1
+  else:
+    return n*factorial(n-1)
 
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
->>> 
+print(factorial(5))
 
 
   
