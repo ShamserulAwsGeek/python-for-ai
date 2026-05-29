3a84825fde211a9142e3fc7155b85e23212e2c8a
Md Shamserul Haque <shamserul786@gmail.com>
2026-05-29 21:17:22 +0530

    Update recursion.py
    fibonacci series

--- recursion.py
+++ recursion.py
@@ -6,5 +6,11 @@ def factorial(n):
 
 print(factorial(5))
 
+#Fibonacci series:
+def fibonacci(n):
+  if(n ==0 or n==1):
+    return n
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
 
-  
+print(fibonacci(4))
