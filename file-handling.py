39809247f4265e1e7b9d6ce49bcbedc5f8d727ba
Md Shamserul Haque <shamserul786@gmail.com>
2026-05-30 21:14:58 +0530

    Update file-handling.py

--- file-handling.py
+++ file-handling.py
@@ -122,3 +122,11 @@ def func():
 
 x = func()
 print(x)
+
+#Raise custom error:
+a = int(input("enter the number between 5 and 9:"))
+
+if(a<5 or a>9):
+  raise ValueError("value is not in range")
+else:
+  print("value is in range")
