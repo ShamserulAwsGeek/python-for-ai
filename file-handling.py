152e7231e4e21e7f31ed4b337356ff8e70439ab6
Md Shamserul Haque <shamserul786@gmail.com>
2026-05-30 20:50:32 +0530

    Update file-handling.py

--- file-handling.py
+++ file-handling.py
@@ -92,3 +92,33 @@ except ValueError:
 
 except IndexError:
   print("Index Error")
+
+
+#Finally Clause:
+
+try:
+  l = [1,3,5,6,7]
+  i = int(input("Enter the index:"))
+  print(l[i])
+except:
+  print("Some error occured")
+
+print("I am always executed!")
+
+
+#Cluase with function:
+def func():
+ try:
+  l = [1,3,5,7]
+  i = int(input("Enter the index:"))
+  print(l[i])
+  return 1
+ except:
+   print("Invalid index")
+   return 0
+
+ finally:
+  print("I am always executed!")
+
+x = func()
+print(x)
