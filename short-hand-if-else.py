e8a82870b59619dea468560aab6ce6719288e547
Md Shamserul Haque <shamserul786@gmail.com>
2026-06-03 23:44:45 +0530

    Update short-hand-if-else.py

--- short-hand-if-else.py
+++ short-hand-if-else.py
@@ -1,3 +1,4 @@
+#If you have one statement for if and one for else, you can put them on the same line using a conditional expression
 a  = 330
 b  = 3303
 print("A") if a > b else print("=") if a == b else print("B")
