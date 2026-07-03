f97081323e0cab1483ce77bc2e480ac107e0d005
Md Shamserul Haque <shamserul786@gmail.com>
2026-07-03 23:54:26 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -33,4 +33,27 @@
 # mytripler = myfunc(3)
 
 # print(mydoubler(11))
-# print(mytripler(11))
+
+
+
+# Lambda Functions in Python
+# A lambda function is a small, anonymous (nameless) function defined in a single line.
+
+#syntax :  lambda arguments : expression
+
+# Regular function
+def add(x, y):
+    return x + y
+
+# Same thing as a lambda
+add = lambda x, y: x + y
+print(add(3, 4))   # 7
+
+
+double = lambda x: x * 2
+cube = lambda x: x ** 3
+avg = lambda x, y, z: (x + y + z) / 3
+
+print(double(5))  # Output: 10
+print(cube(3))    # Output: 27  
+print(avg(4, 5, 6))  # Output: 5.0
