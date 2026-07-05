ae0238963d1db5d6c738b86c5940a6b92d52fb0f
Md Shamserul Haque <shamserul786@gmail.com>
2026-07-05 21:37:24 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -10,5 +10,28 @@ class Person:
         return f"{self.name} is a {self.age} year old {self.occupation} with a net worth of ${self.networth}."
 
 a = Person()
+b = Person()
+b.name = "Maheen"
+b.age = 25
+b.occupation = "Chef"
+b.networth = 50000
 
 print(a.info())
+print(b.info())
+
+#Classes & Objects:
+#A class is a blueprint; an object is an instance of that blueprint.
+class Person:
+    def __init__(self, name, occupation):  # Constructor-  it is also called as Dunder method.
+        self.name = name              # Instance attribute
+        self.occupation = occupation
+
+    def info(self):
+        return f"{self.name} is a {self.occupation}"
+
+# Creating objects:
+a = Person("Shamserul", "Developer")
+b = Person("Maheen", "Chef")
+
+print(a.info())
+print(b.info())
