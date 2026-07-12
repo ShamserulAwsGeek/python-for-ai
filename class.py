c265b97a9a31c0f13d43612c0e132b7abccf799a
Md Shamserul Haque <shamserul786@gmail.com>
2026-07-12 22:58:59 +0530

    Update class.py

--- class.py
+++ class.py
@@ -144,3 +144,24 @@ print(Employee.company_name)
 e2 = Employee("Maheen")
 e2.showDetails()
 
+-----------------------------
+------------------------------
+class Library:
+    def __init__(self):
+        self.no_of_books = 0
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        self.no_of_books = len(self.books)
+
+
+    def showinfo(self):
+        print(f"The library has  {self.no_of_books} books")
+        for book in self.books:
+            print(book)             
+    
+l1 = Library()
+l1.add_book("Python Programming")
+l1.add_book("Data Science")
+l1.showinfo()
