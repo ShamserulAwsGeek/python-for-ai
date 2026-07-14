4a211cf6a4b820fb2f939a98f85da23e4643def7
Md Shamserul Haque <shamserul786@gmail.com>
2026-07-14 22:56:01 +0530

    Update dunder-method.py

--- dunder-method.py
+++ dunder-method.py
@@ -21,3 +21,36 @@ e = Employee("Shamserul")
 print(str(e))
 print(len(e))
 print(e.name)
+
+---------------------------------
+---------------------------------
+class Dog:
+    def __init__(self, name, age):   # called when you create an object
+        self.name = name
+        self.age = age
+
+    def __str__(self):               # called when you print the object
+        return f"{self.name}, Age: {self.age}"
+
+    def __len__(self):               # called when you use len()
+        return self.age
+
+dog = Dog("Bruno", 3)
+
+print(dog)       # Bruno, Age: 3   ← __str__ fired automatically
+print(len(dog))  # 3               ← __len__ fired automatically
+
+----------------------------
+---------------------------
+class Bag:
+    def __init__(self, items):
+        self.items = items
+
+    def __add__(self, other):         # called when you use +
+        return Bag(self.items + other.items)
+
+bag1 = Bag(["apple", "banana"])
+bag2 = Bag(["mango"])
+
+bag3 = bag1 + bag2                    # __add__ fires automatically
+print(bag3.items)  # ["apple", "banana", "mango"]
