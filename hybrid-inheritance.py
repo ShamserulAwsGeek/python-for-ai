0c8f7a1dba9986cb2140fa63628aad8d61cefc85
Md Shamserul Haque <shamserul786@gmail.com>
2026-07-27 22:59:15 +0530

    Create hybrid-inheritance.py

---
+++ hybrid-inheritance.py
@@ -0,0 +1,40 @@
+#Hybrid Inheritance: Hybrid inheritance is a combination of two or more types of inheritance (like multiple,
+#multilevel, and hierarchical) within the same class hierarchy
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print(f"Name: {self.name}")
+
+
+class Employee(Person):
+    def __init__(self, name, employee_id):
+        Person.__init__(self, name)
+        self.employee_id = employee_id
+
+    def show_id(self):
+        print(f"Employee ID: {self.employee_id}")
+        
+
+class Freelancer(Person):
+    def __init__(self, name, project):
+        Person.__init__(self, name)
+        self.project = project
+
+    def show_project(self):
+        print(f"Project: {self.project}")
+
+class Consultant(Freelancer):
+    def __init__(self, name, project, hourly_rate):
+        Freelancer.__init__(self, name, project)
+        self.hourly_rate = hourly_rate
+
+    def show_rate(self):
+        print(f"Hourly Rate: ${self.hourly_rate}")
+
+
+c = Consultant("Shamserul", "AI Development", 50)
+c.display()
+c.show_project()
+c.show_rate()
